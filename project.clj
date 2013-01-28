a692541c88fe5ca71520f236ded0c1dc0ce5e695
matonthecat <matwendt@gmx.de>
2013-01-28 23:43:49 +0100

    added leiningen config

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject matonthecat.clojure/clojure-cluster "0.1.0-SNAPSHOT"
+    :description "Clustering algorithms for Clojure"  
+    :source-paths ["src/main/clojure"]
+    :test-paths  ["src/test/clojure"]
+;    :dev-dependencies []
+    :dependencies [[org.clojure/clojure "1.5.0-RC1"]
+                   [org.clojure/clojure-contrib "1.2.0" :scope "test"]]
+    :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"})
