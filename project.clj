3ec3ee4fda1d42da1064a39c8925a5beec72d429
matonthecat <matwendt@gmx.de>
2013-02-16 18:00:16 +0100

    added incanter for mathematical stuff

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
     :source-paths ["src/main/clojure"]
     :test-paths  ["src/test/clojure"]
 ;    :dev-dependencies []
-    :dependencies [[org.clojure/clojure "1.5.0-RC1"]
+    :dependencies [[org.clojure/clojure "1.5.0-alpha3"]
+                   [incanter/incanter-core "1.4.1"]
                    [org.clojure/clojure-contrib "1.2.0" :scope "test"]]
     :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"})
