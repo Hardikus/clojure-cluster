d8f2b4de8ef57290237149e5cabfc250c6a1f935
matonthecat <matwendt@gmx.de>
2013-02-17 23:12:00 +0100

    made tests work threw away what's obsolete

--- src/test/clojure/cluster/test.clj
+++ src/test/clojure/cluster/test.clj
@@ -5,9 +5,9 @@
     (:use cluster.clustering)
     (:use cluster.core)
     (:use cluster.internal)
+    (:use [clojure.contrib.generic.math-functions :only [approx=]])
     )
 
-(comment "hcluster tests"
 
 (deftest test-hcluster-empty-nodes []
   (is (= [] (hcluster []))))
@@ -19,81 +19,81 @@
 (deftest test-hcluster-two-nodes []
   (is (= '({:left {:vec [1 2]}
             :right {:vec [2 3]}
-            :vec [3/2 5/2]})
+            :vec [1.5 2.5]})
          (hcluster [{:vec [1 2]}
                     {:vec [2 3]}]))))
 
-(deftest test-hcluster-three-nodes []
-  (is (= '({:left {:left {:vec [1 2]} 
-                   :right {:vec [4 8]}
-                   :vec (5/2 5)}
-            :right {:vec [2 3]}
-            :vec (9/4 4)})
-         (hcluster [{:vec [1 2]}
-                    {:vec [2 3]}
-                    {:vec [4 8]}]))))
-)
+(deftest test-hcluster-three-nodes 
+  (let [cluster (hcluster [{:vec [1 2]}
+                           {:vec [2 3]}
+                           {:vec [4 8]}])
+        [{cent1 :vec :as node1}] cluster
+        {{cent2 :vec} :left} node1
+        {{cent3 :vec} :right} node1]
+    (is (= [2.75 5.25] cent1))
+    (is (= [4 8] cent2))
+    (is (= [1.5 2.5] cent3))))
+
+(deftest test-hcluster-map-three-nodes 
+  (let [cluster (hcluster [{:vec {:a 1 :b 2}}
+                           {:vec {:a 2 :b 3}}
+                           {:vec {:a 4 :b 8}}])
+        [{cent1 :vec :as node1}] cluster
+        {{cent2 :vec} :left} node1
+        {{cent3 :vec} :right} node1]
+    (is (= {:a 2.75 :b 5.25} cent1))
+    (is (= {:a 4 :b 8} cent2))
+    (is (= {:a 1.5 :b 2.5} cent3))))
+
 
 ;; kcluster tests
 
-(deftest test-kcluster-two-nodes []
-  (let [[[c1] [c2]] (kcluster [[1 2] [2 1]] 2 1 2)]
-    (is (= c1 0)
-        (= c2 0))))
+(deftest test-k-means-two-nodes
+  (let [[[c1] :as clusters] (k-means [[1 2] [2 1]] 1)]
+    (is (= 1 (count clusters)))
+    (is (= c1 0))))
+
+(deftest test-k-means-three-nodes
+  (let [[[c1] [c2] :as clusters] 
+        (k-means [[1 2] [2 3] [4 8]] 2)]
+    (is (= 2 (count clusters)))
+    (is (= c1 0))
+    (is (= c2 2))))
 
 
 ;; internal tests
 
-(deftest test-sum []
-  (is (== (sum [1 2 3]) 6)))
-
 (deftest test-pearson 
   (is (== 1.0 (pearson [1 2] [1 2])))
   (is (== -1.0 (pearson [1 2] [2 1])))
-  (is (== 0.0 (pearson [1 2 3] [1 2 1]))))
+  (is (== 0.0 (pearson [1 2 3] [1 2 1])))
+  (is (== 1.0 (pearson [1 2 3 5 8] [0.11, 0.12, 0.13, 0.15, 0.18]))))
 
 
 (deftest test-euclidean-distance 
   (is (== 0.0 (euclidean-distance [1 2] [1 2])))
-  (is (== 1.4142135623730951 (euclidean-distance [1 2] [2 1])))
+  (is (approx= 1.414 (euclidean-distance [1 2] [2 1]) 0.001))
   (is (== 3.0 (euclidean-distance [4,1,-2] [2,3,-1])))
-  (is (== 2.0 (euclidean-distance [1 2 3] [1 2 1]))))
-;
-;(deftest test-compact []
-;  (is (== [1 2 3] (compact [nil 1 nil 2 nil 3 nil nil])))
-;
-;(deftest test-average-vectors []
-;  (is (= [2 2 2] (average-vectors [[1 3 1] [3 1 3] [2 2 2]])))
-;
-;(deftest test-grtr []
-;  (is (= 2 (grtr 1 2))
-;  (is (= 2 (grtr 2 1)))
-;
-;(deftest test-closest-vector []
-;  (is (= [0.9999999999999998, 0] (closest-vector [1 2 3] [[1 2 3] [1 2 1]]))
-;  (is (= [0.9999999999999998, 1] (closest-vector [1 2 3] [[1 2 1] [1 2 3]]))
-;  (is (= [0.0, 1] (closest-vector [1 2 3] [[3 2 1] [1 2 1]])))
-;
-;(deftest test-closest-vectors []
-;  (is (= [[0 1] 0.9999999999999998] (closest-vectors [[1 2 3] [1 2 3] [1 2 1]]))
-;  (is (= [[0 2] 0.9999999999999998] (closest-vectors [[1 2 3] [1 2 1] [1 2 3]]))
-;  (is (= [[1 2] 0.9999999999999998] (closest-vectors [[1 2 1] [1 2 3] [1 2 3]])))
-;
-;(deftest test-include? []
-;  (assert-true (include? [1 2 3] 1))
-;  (assert-nil (include? [1 2 3] 4)))
-;
-;(deftest test-without []
-;  (is (= [1 2 4] (without [1 2 3 4] 2))
-;  (is (= [1 4] (without [1 2 3 4] 2 1)))
-;
-;(deftest test-random-vector []
-;  (let [r (random-vector 5 0 1)]
-;    (is (= 5 (count r))
-;    (is (= true (every? #(< % 1) r))
-;    (is (= true (every? #(> % 0) r))))
-;
-;(deftest test-random-vectors []
-;  (let [rs (random-vectors 4 5 0 1)]
-;    (is (= 4 (count rs))
-;    (assert-true (every? #(= 5 (count %)) rs))))
+  (is (== 2.0 (euclidean-distance [1 2 3] [1 2 1])))
+  (is (approx= 9.860
+          (euclidean-distance [1 2 3 5 8] [0.11, 0.12, 0.13, 0.15, 0.18])
+          0.001)))
+
+(deftest test-centroid
+  (is (= [2.0 2.0 2.0] (centroid [1 3 1] [3 1 3] [2 2 2])))
+  (is (= 3 (count (centroid [1 3 1] [3 1 3] [2 2]))))
+  (is (= 3 (count (centroid [1 3] [3 1 3] [2 2 2]))))
+  (is (= {:a 2.0 :b 2.0 :c 2.0} (centroid {:a 1 :b 3 :c 1} {:a 3 :b 1 :c 3} {:a 2 :b 2 :c 2}))))
+
+(deftest test-min-distance
+  (is (= [1 2 3] (min-distance [1 2 3] [[1 2 3] [1 2 1]])))
+  (is (= [1 2 3] (min-distance [1 2 3] [[1 2 1] [1 2 3]])))
+  (is (= [1 2 1] (min-distance [1 2 3] [[3 2 1] [1 2 1]]))))
+
+;; WARN: pearson correlation is at max when the vectors are similar
+;; whereas other similarity measures like euclidian distance are at min
+(deftest test-closest-vectors []
+  (is (= [0 1] (closest-vectors [[1 2 3] [1 2 3] [1 2 1]])))
+  (is (= [0 2] (closest-vectors [[1 2 3] [1 2 1] [1 2 3]])))
+  (is (= [1 2] (closest-vectors [[1 2 1] [1 2 3] [1 2 3]]))))
+
